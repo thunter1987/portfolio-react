c12dd2bd143f4906bfe96d62ecbe7a7ba7fa9a1e
Tony Hunter <thunter1987@yahoo.com>
2023-11-24 13:38:52 -0600

    feat: Creates Usercontroller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -51,6 +51,38 @@ const createNewUser = asyncHandler(async (req, res) => {
 // @route PATCH /users
 // @access Private
 const updateUser = asyncHandler(async (req, res) => {
+    const {id, username, roles, active, password} = req.body
+    
+    //Confirm data
+    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+        return res.status(400).json({message: 'All fields are required'})
+    }
+
+    const user = await User.findById(id).exec
+
+    if(!user) {
+        return res.status(400).json({message: 'User not found'})
+    }
+
+    //Check for duplicate
+    const duplicate = await User.findOne({username}).lean().exec
+    //Allow updates to the original user
+    if(duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({message: 'Duplicate username'})
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+
+    if (password) {
+        //Hash password
+        user.password = await bcrypt.hash(password, 10)
+
+        const updatedUser = await user.save()
+
+        res.json({message: `${updatedUser.username} updated`})
+    }
 
 })
 
@@ -58,7 +90,28 @@ const updateUser = asyncHandler(async (req, res) => {
 // @route DELETE /users
 // @access Private
 const deleteUser = asyncHandler(async (req, res) => {
+    const {id} = req.body
 
+    if (!id) {
+        return res.status(400).json({message: 'User ID required'})
+    }
+
+    const notes = await Note.findOne({user: id}).lean().exec()
+    if (notes?.length) {
+        return res.status(400).json({message: 'User has assigned notes'})
+    }
+
+    const user = await User.findById(id).exec()
+
+    if (!user) {
+        return res.status(400).json({message: 'User not found'})
+    }
+
+    const result = await user.deleteone()
+
+    const reply = `Username ${result.username} with ID ${result._id} deleted`
+
+    res.json(reply)
 })
 
 module.exports = {
