aa62ff9c01c3523225026578c4d3e6d5b9efd39c
Tony Hunter <thunter1987@yahoo.com>
2023-11-24 13:45:22 -0600

    feat: fixes Get method for users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,122 +1,121 @@
-const User = require('../models/User')
-const Note = require('../models/Note')
-const asyncHandler = require('express-async-handler')
-const bcrypt = require('bcrypt')
+const User = require("../models/User");
+const Note = require("../models/Note");
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
 
 // @desc Get All users
 // @route GET /users
 // @access Private
 const getAllUsers = asyncHandler(async (req, res) => {
-    const users = await User.find().select('-password').lean()
-    if(!users) {
-        return res.status(400).json({message: 'No users found'})
-    }
-    res.json(users)
-})
+  const users = await User.find().select("-password").lean();
+  if (!users?.length) {
+    return res.status(400).json({ message: "No users found" });
+  }
+  res.json(users);
+});
 
 // @desc Create new user
 // @route POST /users
 // @access Private
 const createNewUser = asyncHandler(async (req, res) => {
-    const {username, password, roles } = req.body
+  const { username, password, roles } = req.body;
 
-    //Confirm data
-    if(!username || !password || !Array.isArray(roles) || !roles.length) {
-        return res.status(400).json({message: 'All fields are required'})
-    }
+  //Confirm data
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-    //Check for duplicate
-    const duplicate = await User.findOne({username}).lean().exec()
+  //Check for duplicate
+  const duplicate = await User.findOne({ username }).lean().exec();
 
-    if (duplicate) {
-        return res.status(409).json({message: 'Duplicate username'})
-    }
+  if (duplicate) {
+    return res.status(409).json({ message: "Duplicate username" });
+  }
 
-    //Hash password
-    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
+  //Hash password
+  const hashedPwd = await bcrypt.hash(password, 10); // salt rounds
 
-    const userObject = {username, 'password': hashedPwd, roles}
+  const userObject = { username, password: hashedPwd, roles };
 
-    //Create and store new user
-    const user = await User.create(userObject)
+  //Create and store new user
+  const user = await User.create(userObject);
 
-    if (user) {
-        res.status(201).json({message: `New user ${username} created`})
-    } else {
-        res.status(400).json({message: 'Invalid user data received'})
-    }
-})
+  if (user) {
+    res.status(201).json({ message: `New user ${username} created` });
+  } else {
+    res.status(400).json({ message: "Invalid user data received" });
+  }
+});
 
 // @desc Update a user
 // @route PATCH /users
 // @access Private
 const updateUser = asyncHandler(async (req, res) => {
-    const {id, username, roles, active, password} = req.body
-    
-    //Confirm data
-    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
-        return res.status(400).json({message: 'All fields are required'})
-    }
+  const { id, username, roles, active, password } = req.body;
 
-    const user = await User.findById(id).exec
+  if (
+    !id ||
+    !username ||
+    !Array.isArray(roles) ||
+    !roles.length ||
+    typeof active !== "boolean"
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-    if(!user) {
-        return res.status(400).json({message: 'User not found'})
-    }
+  const user = await User.findById(id).exec;
 
-    //Check for duplicate
-    const duplicate = await User.findOne({username}).lean().exec
-    //Allow updates to the original user
-    if(duplicate && duplicate?._id.toString() !== id) {
-        return res.status(409).json({message: 'Duplicate username'})
-    }
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
 
-    user.username = username
-    user.roles = roles
-    user.active = active
+  const duplicate = await User.findOne({ username }).lean().exec;
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: "Duplicate username" });
+  }
 
-    if (password) {
-        //Hash password
-        user.password = await bcrypt.hash(password, 10)
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
 
-        const updatedUser = await user.save()
-
-        res.json({message: `${updatedUser.username} updated`})
-    }
-
-})
+  if (password) {
+    user.password = await bcrypt.hash(password, 10);
+    const updatedUser = await user.save();
+    res.json({ message: `${updatedUser.username} updated` });
+  }
+});
 
 // @desc Delete a user
 // @route DELETE /users
 // @access Private
 const deleteUser = asyncHandler(async (req, res) => {
-    const {id} = req.body
+  const { id } = req.body;
 
-    if (!id) {
-        return res.status(400).json({message: 'User ID required'})
-    }
+  if (!id) {
+    return res.status(400).json({ message: "User ID required" });
+  }
 
-    const notes = await Note.findOne({user: id}).lean().exec()
-    if (notes?.length) {
-        return res.status(400).json({message: 'User has assigned notes'})
-    }
+  const notes = await Note.findOne({ user: id }).lean().exec();
+  if (notes?.length) {
+    return res.status(400).json({ message: "User has assigned notes" });
+  }
 
-    const user = await User.findById(id).exec()
+  const user = await User.findById(id).exec();
 
-    if (!user) {
-        return res.status(400).json({message: 'User not found'})
-    }
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
 
-    const result = await user.deleteone()
+  const result = await user.deleteone();
 
-    const reply = `Username ${result.username} with ID ${result._id} deleted`
+  const reply = `Username ${result.username} with ID ${result._id} deleted`;
 
-    res.json(reply)
-})
+  res.json(reply);
+});
 
 module.exports = {
-    getAllUsers,
-    createNewUser,
-    updateUser,
-    deleteUser
-}
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser,
+};
