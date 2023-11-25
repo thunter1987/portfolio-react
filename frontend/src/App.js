1dbb569c2992313649c8954d0afa2af9ce473b75
Tony Hunter <thunter1987@yahoo.com>
2023-11-25 13:37:20 -0600

    feat: initial Frontend commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
 
 function App() {
-  return (<>
-  </>
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Public />} />
+      <Route path="login" element={<Login />} />
+    
+    <Route path="dash" element={<DashLayout />}>
+
+      </Route>
+
+      </Route>
+    </Routes>
   );
 }
 
---
+++ frontend/src/components/Public.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+const Public = () => {
+  const content = (
+    <section className="public">
+      <header>
+        <h1>
+          Welcome to <span className="nowrap">Tony's Portfolio App Page!</span>
+        </h1>
+      </header>
+      <main className="public_main">
+        <p>A recent graduate of Flatiron School</p>
+        <address className="public_addr">
+          Tony Hunter
+          <br />
+          1234 Any Street
+          <br />
+          Conroe, TX 77304
+          <br />
+          <a href="tel:+18325252465">(832) 525-2465</a>
+        </address>
+        <br />
+        <p>Owner: Tony Hunter</p>
+      </main>
+      <footer>
+        <Link to="/login">Employee Login</Link>
+      </footer>
+    </section>
+  );
+  return content;
+};
+
+export default Public;
