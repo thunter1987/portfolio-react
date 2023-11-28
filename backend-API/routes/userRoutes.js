2b2b56d2a11b34bc97e60c69bc708324390dabcc
Tony Hunter <thunter1987@yahoo.com>
2023-11-28 10:04:05 -0600

    config: cleans up code formatting

--- backend-API/routes/userRoutes.js
+++ backend-API/routes/userRoutes.js
@@ -1,12 +1,12 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const usersController = require("../controllers/usersController")
+const usersController = require('../controllers/usersController')
 const verifyJWT = require('../middleware/verifyJWT')
 
 router.use(verifyJWT)
 
 router
-  .route("/")
+  .route('/')
   .get(usersController.getAllUsers)
   .post(usersController.createNewUser)
   .patch(usersController.updateUser)
