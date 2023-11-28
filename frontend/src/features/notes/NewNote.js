9059a224e3e4d142ebd512c0a2f7816a9d9f5c8e
Tony Hunter <thunter1987@yahoo.com>
2023-11-28 10:22:58 -0600

    feat: Login Authentication finished

--- frontend/src/features/notes/NewNote.js
+++ frontend/src/features/notes/NewNote.js
@@ -1,10 +1,12 @@
-import { useSelector } from "react-redux";
-import { selectAllUsers } from "../users/usersApiSlice";
-import NewNoteForm from "./NewNoteForm";
+import { useSelector } from 'react-redux'
+import { selectAllUsers } from '../users/usersApiSlice'
+import NewNoteForm from './NewNoteForm'
 
 const NewNote = () => {
-  const users = useSelector(selectAllUsers);
-  const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>;
-  return content;
-};
-export default NewNote;
+  const users = useSelector(selectAllUsers)
+
+  if (!users.length) return <p>Not Currently Available</p>
+  const content = <NewNoteForm users={users} />
+  return content
+}
+export default NewNote
