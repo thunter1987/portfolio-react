2444d4be01352c7e073d7a056edd479b3e95dd1d
Tony Hunter <thunter1987@yahoo.com>
2023-11-30 16:07:55 -0600

    config: fixes CORS and Query URL

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://tonys-project-portfolio-notesAPI.onrender.com/',
+  baseUrl: 'https://tonys-project-portfolio-notes-api.onrender.com/',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token
